fbe162f9981ba84c5e539f4067b2246f057f6dcf
IwnuplyNotTyan <ikissiwnuply@gmail.com>
2026-08-25 20:23:19 +0300

    feat: some themes update (i cant fix gtk4 theme kekb)

--- home/anewaqq/module/wm/theme.nix
+++ home/anewaqq/module/wm/theme.nix
@@ -13,10 +13,34 @@ gtk = {
     name = "Iosevka Nerd Font";
     size = 11;
   };
+
+  theme = {
+    name = "Adwaita-dark";
+    package = pkgs.gnome-themes-extra;
+  };
   
   gtk3.extraConfig = {
     gtk-application-prefer-dark-theme = true;
   };
+
+  gtk4 = {
+  theme = null;
+  extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+  };
+  };
+};
+
+dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
+xdg.portal = {
+  enable = true;
+  extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  config.common.default = "*";
 };
 
 home.sessionVariables = {
