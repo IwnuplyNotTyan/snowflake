52eff3fc4cec68a7eb6b26e4080b742d43fe7b76
IwnuplyNotTyan <ikissiwnuply@gmail.com>
2026-08-28 20:58:39 +0300

    remove: soft alias

--- home/anewaqq/module/ssh/default.nix
+++ home/anewaqq/module/ssh/default.nix
@@ -37,11 +37,11 @@ in
         identityFile = "~/.ssh/id_ed25519";
       };
 
-      "Soft" = {
-        hostname = "git.welara-sun.ts.net";
-        port = 23231;
-        identityFile = "~/.ssh/id_ed25519";
-      };
+      #"Soft" = {
+      #  hostname = "git.welara-sun.ts.net";
+      #  port = 23231;
+      #  identityFile = "~/.ssh/id_ed25519";
+      #};
 
       "Mikrotik" = {
         hostname = "192.168.1.1";
